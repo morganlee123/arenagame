75d4ce0b6f69a3794ac75360bd9bec62042e0fac
morganlee123 <msandler8@gmail.com>
2017-04-20 08:42:19 -0400

    Add files via upload

--- Engine/com/nexide/main/gfx/ImageManager.java
+++ Engine/com/nexide/main/gfx/ImageManager.java
@@ -1,5 +1,8 @@
 package com.nexide.main.gfx;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class ImageManager {
@@ -16,8 +19,18 @@ public class ImageManager {
 	}
 	
 	public void loadImages(){
-		grassTileset[0] = ss.crop(1, 1, DEFAULT_TILESIZE, DEFAULT_TILESIZE); // plain grass tile
 		
+		/* ADD NEW TILESETS, EVERYTHING BELOW IS TEMPORARY*/
+		grassTileset[0] = ss.crop(1, 1, DEFAULT_TILESIZE, DEFAULT_TILESIZE);
+		grassTileset[0] = createResizedCopy(grassTileset[0], 64, 64); //grass
+		
+		grassTileset[1] = ss.crop(7,1, DEFAULT_TILESIZE, DEFAULT_TILESIZE);
+		grassTileset[1] = createResizedCopy(grassTileset[1], 64, 64); //transfer from grass to sand
+	
+		grassTileset[2] = ss.crop(6,1, DEFAULT_TILESIZE, DEFAULT_TILESIZE);
+		grassTileset[2] = createResizedCopy(grassTileset[2], 64, 64); //sand
+		
+		/****************************************************/
 	}
 
 	public BufferedImage[] getTileset(String requestedTileset){
@@ -28,5 +41,16 @@ public class ImageManager {
 		System.out.println("Not a valid tileset");
 		return null;
 	}
+	
+	public BufferedImage createResizedCopy(Image originalImage, 
+    		int scaledWidth, int scaledHeight)
+    {
+    	System.out.println("resizing...");
+    	BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_ARGB);
+    	Graphics2D g = scaledBI.createGraphics();
+    	g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null); 
+    	g.dispose();
+    	return scaledBI;
+    }
 	
 }
