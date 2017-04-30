b95a3486f51bfa6671960e32def294c123530962
pateljo <pateljo@northvilleschools.net>
2017-04-30 13:41:43 -0400

    Update ImageLoader.java

--- Engine/com/nexide/main/gfx/ImageLoader.java
+++ Engine/com/nexide/main/gfx/ImageLoader.java
@@ -1,10 +1,11 @@
 package com.nexide.main.gfx;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+
+import com.nexide.main.Game;
 
 public class ImageLoader {
 
@@ -14,7 +15,7 @@ public class ImageLoader {
 	
 	public BufferedImage loadImage(String path){
 		try {
-			return ImageIO.read(new File(path));
+			return ImageIO.read(Game.class.getResource(path));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
